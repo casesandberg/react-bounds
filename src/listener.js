408db0b3d51a7e5ad2a3009a1222483dbd20638a
case <case@casesandberg.com>
2015-07-13 00:06:38 -0700

    listener cleanup

--- src/listener.js
+++ src/listener.js
@@ -3,53 +3,67 @@
 
 // http://www.backalleycoder.com/2013/03/18/cross-browser-event-based-element-resize-detection/
 
-var requestFrame = (function(){
-    var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame ||
-        function(callback){ return window.setTimeout(callback, 20); };
-    return function(callback){ return raf(callback); };
-  })();
+// http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
+var requestFrame = function(callback){
+  var raf = window.requestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            function(callback){
+              return window.setTimeout(callback, 1000 / 60);
+            };
 
-var cancelFrame = (function(){
-  var cancel = window.cancelAnimationFrame || window.mozCancelAnimationFrame || window.webkitCancelAnimationFrame ||
-         window.clearTimeout;
-  return function(id){ return cancel(id); };
-})();
+  return raf(callback);
+};
+
+var cancelFrame = function(id){
+  var cancel = window.cancelAnimationFrame ||
+               window.mozCancelAnimationFrame ||
+               window.webkitCancelAnimationFrame ||
+               window.clearTimeout;
+
+  return cancel(id);
+};
 
 function resizeListener(e) {
-  var win = e.target || e.srcElement;
-  if (win.__resizeRAF__) cancelFrame(win.__resizeRAF__);
-  win.__resizeRAF__ = requestFrame(function(){
-    var trigger = win.__resizeTrigger__;
-    trigger.__resizeListeners__.forEach(function(callback){
+  var element = e.target || e.srcElement;
+  if (element.resizeAnimation) {
+    cancelFrame(element.resizeAnimation);
+  };
+  element.resizeAnimation = requestFrame(function() {
+    var trigger = element.resizeTrigger;
+    trigger.resizeListener.forEach(function(callback) {
       callback.call(trigger, e);
     });
   });
 }
 
 function objectLoad(e) {
-  this.contentDocument.defaultView.__resizeTrigger__ = this.__resizeElement__;
+  this.contentDocument.defaultView.resizeTrigger = this.resizeElement;
   this.contentDocument.defaultView.addEventListener('resize', resizeListener);
 }
 
-function addResizeTrigger(element) {
-  var obj = element.__resizeTrigger__ = document.createElement('object');
+function addChildTrigger(element) {
+  var obj = document.createElement('object');
   obj.setAttribute('style', 'display: block; position: absolute; top: 0; left: 0; height: 100%; width: 100%; overflow: hidden; pointer-events: none; z-index: -1;');
-  obj.__resizeElement__ = element;
+  obj.resizeElement = element;
   obj.onload = objectLoad;
   obj.data = 'about:blank';
+
   element.appendChild(obj);
+  element.resizeTrigger = obj;
 }
 
 module.exports = {
   add: function(element, callback){
-    if (!element.__resizeListeners__) {
-      element.__resizeListeners__ = [];
-      addResizeTrigger(element);
+    if (!element.resizeListener) {
+      element.resizeListener = [];
+      addChildTrigger(element);
     }
-    element.__resizeListeners__.push(callback);
+    element.resizeListener.push(callback);
+    console.log(element);
   },
 
   remove: function(element, callback){
-    element.__resizeListeners__.splice(element.__resizeListeners__.indexOf(callback), 1);
+    element.resizeListener.splice(element.resizeListener.indexOf(callback), 1);
   }
 };
