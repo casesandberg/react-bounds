ed01b280e2589dfbc31da0e8746934a8f8ee2372
case <case@casesandberg.com>
2015-09-17 01:33:44 -0400

    Documentation

--- docs/documentation/index.js
+++ docs/documentation/index.js
@@ -5,4 +5,9 @@ module.exports = {
   '02.01-install': require('./02.01-install.md'),
   '02.02-wrap': require('./02.02-wrap.md'),
   '02.03-define-bounds': require('./02.03-define-bounds.md'),
+  '03-props': require('./03-props.md'),
+  '03.01-active': require('./03.01-active.md'),
+  '03.02-isBound': require('./03.02-isBound.md'),
+  '03.03-width-height': require('./03.03-width-height.md'),
+  '04-bounds-method': require('./04-bounds-method.md'),
 };
