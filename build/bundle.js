2cb29ecc38d8c19cba13b795027051efa2c23194
case <case@casesandberg.com>
2015-09-20 16:24:17 -0500

    Docs Update

--- build/bundle.js
+++ build/bundle.js
@@ -42844,7 +42844,7 @@
 /* 201 */
 /***/ function(module, exports) {
 
-	module.exports = "---\nid: methodology\ntitle: Methodology\n\n---\nTruly modular components should conform and adapt to whatever size box you give them. To achieve this, top-level elements in components should have a fluid width and height. Components should strive to fill 100% of the space they are given, and leave the burden of constraining them to the parent.\n\nTake, for example, our Schedule component: I am rendering the schedule in a div that has a fixed height and width, and the scheduler fills up the available space. Similarly, If you were to render that scheduler in an empty body, it would fill the entire page. \n";
+	module.exports = "---\nid: methodology\ntitle: Methodology\n\n---\nTruly modular components should conform and adapt to whatever size box you give them. To achieve this, top-level elements in components should have a fluid width and height. Components should strive to fill 100% of the space they are given, and leave the burden of constraining them to the parent.\n\nTake, for example, the Schedule component above: It is rendered in a div that has a fixed height and width. The scheduler fills up the available space. Similarly, if you were to render that scheduler in an empty body, it would fill the entire page.\n";
 
 /***/ },
 /* 202 */
