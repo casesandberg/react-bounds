29f5785bd40db3688ecb8de16fdeaeb8b75332de
case <case@casesandberg.com>
2016-11-11 16:38:11 -0800

    Update to Support React 15

--- src/Wrap.js
+++ src/Wrap.js
@@ -1,14 +1,12 @@
 'use strict'
 
 var React = require('react')
-var ReactCSS = require('reactcss')
 var listener = require('./listener')
 var _ = require('underscore')
 var classNames = require('classnames')
 
 module.exports = function (Component) {
-  class Wrap extends ReactCSS.Component {
-
+  class Wrap extends React.Component {
     constructor() {
       super()
 
@@ -27,22 +25,8 @@ module.exports = function (Component) {
       this.handleLookup = this.handleLookup.bind(this)
     }
 
-    classes() {
-      return {
-        'default': {
-          wrap: {
-            position: 'relative',
-            height: '100%',
-          },
-          component: {
-            height: '100%',
-          },
-        },
-      }
-    }
-
     handleResize(log) {
-      var component = React.findDOMNode(this.refs.wrap)
+      var component = this.wrap
       this.setState({ loaded: true, debounced: false, width: component.clientWidth, height: component.clientHeight, activeBounds: this.calculateBounds(component.clientWidth, component.clientHeight) })
       this.debounce()
     }
@@ -82,8 +66,8 @@ module.exports = function (Component) {
     }
 
     componentDidMount() {
-      var component = React.findDOMNode(this.refs.component)
-      listener.add(component, this.handleResize)
+      var component =
+      listener.add(this.component, this.handleResize)
 
       if (!this.state.loaded) {
         this.handleResize()
@@ -99,8 +83,7 @@ module.exports = function (Component) {
     }
 
     componentWillUnmount() {
-      var component = React.findDOMNode(this.refs.component)
-      listener.remove(component, this.handleResize)
+      listener.remove(this.component, this.handleResize)
     }
 
     handleLookup(bound) {
@@ -112,9 +95,22 @@ module.exports = function (Component) {
     }
 
     render() {
+      const styles = {
+        wrap: {
+          position: 'relative',
+          height: '100%',
+        },
+        component: {
+          height: '100%',
+        },
+      }
       return (
-        <div ref="wrap" style={ this.styles().wrap }>
-          <div ref="component" className={ classNames(this.state.activeBounds) } style={ this.styles().component }>
+        <div ref={ wrap => (this.wrap = wrap) } style={ styles.wrap }>
+          <div
+            ref={ component => (this.component = component) }
+            className={ classNames(this.state.activeBounds) }
+            style={ styles.component }
+          >
             { this.state.loaded && this.state.width > 0 ?
               <Component {...this.props }
                 width={ this.state.width }
